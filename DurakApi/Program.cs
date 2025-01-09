70637ea3238035244033b1e4eaa41279efdc65c6
Tobias Fischer <fischerstobi1998@gmail.com>
2025-01-09 16:45:07 +0100

    Add project files.

---
+++ DurakApi/Program.cs
@@ -0,0 +1,53 @@
+using DotNetEnv;
+using Serilog;
+using Serilog.Events;
+
+Env.TraversePath().Load();
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File(
+        Env.GetString("log_path"), 
+        restrictedToMinimumLevel: LogEventLevel.Information,
+        rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 30)
+    .CreateLogger();
+
+try {
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddSerilog();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddSignalR();
+
+    builder.Services.AddAuthentication();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+} catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+} finally
+{
+    Log.CloseAndFlush();
+}
+
